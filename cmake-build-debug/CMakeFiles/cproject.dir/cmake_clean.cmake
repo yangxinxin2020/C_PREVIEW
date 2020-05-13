8e038be615aabac5a6f527973173c8bb4b29286a
yangxinxin2020 <m13289279907@163.com>
2020-05-13 12:56:48 +0800

    提交人：杨鑫鑫 提交信息：整合CLion&GitHub

---
+++ cmake-build-debug/CMakeFiles/cproject.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cproject.dir/main.cpp.obj"
+  "cproject.exe"
+  "cproject.exe.manifest"
+  "cproject.pdb"
+  "libcproject.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cproject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/cproject.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for cproject.
+# This may be replaced when dependencies are built.
